ef787b2bc25c806f06acf541b55655ce235e8011
Jose Garcia <josegarcia1006@gmail.com>
2023-11-24 15:52:01 +0100

    Reto 6 avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,132 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './index.css';
 
-import './App.css'
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [editedTodo, setEditedTodo] = useState(null);
 
-function App() {
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    } else {
+      fetchTodos();
+    }
+  }, []);
+
+  const fetchTodos = () => {
+    axios.get('http://localhost:3000/todo')
+      .then(response => {
+        setTodos(response.data);
+        saveTodosToLocalStorage(response.data);
+      })
+      .catch(error => console.error('Error fetching todos:', error));
+  };
+
+  const saveTodosToLocalStorage = (todos) => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const handleAddTodo = () => {
+    axios.post('http://localhost:3000/todo', { text: newTodo, fecha: '', done: false })
+      .then(response => {
+        const updatedTodos = [...todos, response.data];
+        setTodos(updatedTodos);
+        saveTodosToLocalStorage(updatedTodos);
+        setNewTodo('');
+      })
+      .catch(error => console.error('Error adding todo:', error));
+  };
+
+  const handleEditTodo = (id, newText, newDone) => {
+    axios.patch(`http://localhost:3000/todo/${id}`, { text: newText, done: newDone })
+      .then(response => {
+        const updatedTodos = todos.map(todo => (todo.id === id ? response.data : todo));
+        setTodos(updatedTodos);
+        saveTodosToLocalStorage(updatedTodos);
+        setEditedTodo(null);
+      })
+      .catch(error => console.error('Error editing todo:', error));
+  };
+
+  const handleDeleteTodo = (id) => {
+    axios.delete(`http://localhost:3000/todo/${id}`)
+      .then(() => {
+        const updatedTodos = todos.filter(todo => todo.id !== id);
+        setTodos(updatedTodos);
+        saveTodosToLocalStorage(updatedTodos);
+      })
+      .catch(error => console.error('Error deleting todo:', error));
+  };
+
+  const handleToggleDone = (id, currentDone) => {
+    const newDone = !currentDone;
+    const updatedTodos = todos.map(todo => (todo.id === id ? { ...todo, done: newDone } : todo));
+    setTodos(updatedTodos);
+    saveTodosToLocalStorage(updatedTodos);
+  };
+
+  const handleEditTodoStart = (id) => {
+    setNewTodo(todos.find(todo => todo.id === id).text);
+    setEditedTodo(id);
+  };
 
   return (
     <div>
-      YOUR APP HERE
+      <h1>House cleaning checklist</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button
+        onClick={handleAddTodo}
+        disabled={editedTodo !== null}
+        className={`add-button`}
+      >
+        Add New Task
+      </button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id} className={todo.done ? 'done' : ''}>
+            <>
+              {editedTodo === todo.id ? (
+                <>
+                  <input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    className={`input`}
+                  />
+                  <button onClick={() => handleEditTodo(todo.id, newTodo, todo.done)} className={`button`}>
+                    Save
+                  </button>
+                </>
+              ) : (
+                <>
+                  {todo.text}
+                  <div className="button-container">
+                    <button onClick={() => handleEditTodoStart(todo.id)} disabled={editedTodo !== null} className={`button`}>
+                      Edit
+                    </button>
+                    <button onClick={() => handleDeleteTodo(todo.id)} disabled={editedTodo !== null} className={`button`}>
+                      Delete
+                    </button>
+                    <button onClick={() => handleToggleDone(todo.id, todo.done)} className={`button`}>
+                      {todo.done ? 'Undo' : 'Done'}
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from 'react';
+import { createRoot } from 'react-dom/client';  
+import App from './App';
+
+import './index.css';
+
+const root = document.getElementById('root');
+
+const reactRoot = createRoot(root);
+
+reactRoot.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
